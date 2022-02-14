43f2ca150c2a64d5299e273f0aa6380c3b42e9db
oumaima <oumaima.ayachi@esprit.tn>
2022-02-14 02:08:18 +0100

    Merge branch 'main' into oumaima

--- PiDev/src/Controller/PanierController.php
+++ PiDev/src/Controller/PanierController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Panier;
 
 class PanierController extends AbstractController
 {
@@ -27,5 +28,12 @@ class PanierController extends AbstractController
         return $this->render("panier/panier.html.twig", ["panier" => $panier]);
     }
 
+    /**
+     * @Route("/showPanierAdmin", name="showPanierAdmin");
+     */
+    public function showPanierAdmin(){
+        return $this->render("panier/gestionPanier.html.twig", [
+        ]);
+    }
 
 }
--- PiDev/src/Entity/Panier.php
+++ PiDev/src/Entity/Panier.php
@@ -16,32 +16,7 @@ class Panier
      * @ORM\Column(type="integer")
      */
     private $id;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="panier")
-     */
-    private $user_panier;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $prix; 
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $num_panier;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $quantite;
-
-    /**
-     * @ORM\Column(type="date")
-     */
-    private $date_Ajout;
-
+    
     /**
      * @ORM\Column(type="float")
      */
@@ -52,54 +27,6 @@ class Panier
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getPrix(): ?float
-    {
-        return $this->prix;
-    }
-
-    public function setPrix(float $prix): self
-    {
-        $this->prix = $prix;
-
-        return $this;
-    }
-
-    public function getNum_panier(): ?int
-    {
-        return $this->prix;
-    }
-
-    public function setNum_panier(int $num_panier): self
-    {
-        $this->num_panier = $num_panier;
-
-        return $this;
-    }
-
-    public function getQuantite(): ?int
-    {
-        return $this->quantite;
-    }
-
-    public function setQuantite(int $quantite): self
-    {
-        $this->quantite = $quantite;
-
-        return $this;
-    }
-    
-    public function getDate_Ajout(): ?Date
-    {
-        return $this->date_Ajout;
-    }
-
-    public function setDate_Ajout(Date $date_Ajout): self
-    {
-        $this->date_Ajout = $date_Ajout;
-
-        return $this;
     }
     
     public function getPrix_totale(): ?float
